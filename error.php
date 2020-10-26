02d2b6d7efdf19fbb9f833ecad00590f1f8dd69f
PabloGomezB <a18pabgombra@inspedralbes.cat>
2020-10-26 14:31:25 +0100

    error.php actualizado

--- error.php
+++ error.php
@@ -1,1 +1,42 @@
-ESTO ES LA PAGINA DE ERROR
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Error</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="./css/error.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
+
+<body>
+
+	<div id="error">
+		<div class="error">
+			<div class="errorCookie">
+				<h1>Oops!</h1>
+			</div>
+			<h2>Máximo de pedidos realizados</h2>
+            <p>Solo se permite realizar un pedido cada 24 horas.<br>Lamentamos las molestias.</p>
+			<a href="./index.php">Volver</a>
+		</div>
+	</div>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
--- index.php
+++ index.php
@@ -14,16 +14,19 @@
     <!-- Este script se ejecuta cuando el user hace click en el boton entrar -->
     <?php
 
-        // 
+        // Si el usuario ha clickado en el boton "entrar" se ejecuta la funcion check()
         if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['entrar'])){
             check();
         }
 
+        // Esta funcion revisa si hay una cookie con el timepo ya creada
+        // Si existe redirige a la pagina error.php
+        // Si no existe la crea para las proximas 24h y redirige al menu.php
         function check(){
             $tiempo = date('d/m/Y');
 
             if (!isset($_COOKIE['fecha'])) {
-                setcookie("fecha", $tiempo, time() + 60*60*24);
+                setcookie("fecha", $tiempo, time() + 24 * 3600);
                 header("Location: ./menu.php");
                 die();
             }else{
@@ -35,6 +38,7 @@
         
     ?>
 
+    <!-- Div del banner -->
     <div id="header">
         <h1 style="font-size:80px">Bienvenido</h1>
         <h1>Cantina Kolvin</h1>
