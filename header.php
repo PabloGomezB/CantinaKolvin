b6af17fa87c6d673dd1e7e695e3df9aeb77e896f
PabloGomezB <a18pabgombra@inspedralbes.cat>
2020-10-27 14:32:46 +0100

    header en index y menu arreglado

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light" style="background-color: #e3f2fd;">
+<nav class="navbar navbar-expand-md navbar-light fixed-top" style="background-color: #e3f2fd;">
     <a class="navbar-brand" href="#"><img src="./img/logo.png" width="130" height="60"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- menu.php
+++ menu.php
@@ -3,12 +3,20 @@
 <head>
     <link rel="stylesheet" href="css/menu.css">
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <script type="text/javascript" src="js/menu_funcions.js"></script>
 
 </head>
 
 <body>
+
+    <header style="margin-bottom: 80px;">
+        <!-- <div style="position: fixed; width: 100%"> -->
+        <?php include("header.php"); ?>
+
+    </header>
+
     <div id="mySidebar" class="sidebar">
         <form method="post" action="finalizacion.php">
             <input type="hidden" name="arrayComanda" id="arrayComanda" value="">
@@ -21,6 +29,7 @@
         <div id="items"></div>
     </div>
 
+    <?php include("footer.php"); ?>
 </body>
 
 </html>
