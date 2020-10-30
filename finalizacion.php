40555072437c6061fbe90e3703dfd688e65d821e
PabloGomezB <a18pabgombra@inspedralbes.cat>
2020-10-30 15:00:45 +0100

    formulario creado en finalizacion.php

--- finalizacion.php
+++ finalizacion.php
@@ -25,11 +25,24 @@
     <div id="tabla"></div>
 
     <br>
-    <form action="confirmacion.php" method="POST">
+        
+  
+    <form action="confirmacion.php" method="POST"  class="form">
+        <img src="img/finalizacion/contacto.png" class="formImg"/>
+        <label>Nombre y apellidos: <span>*</span></label>
+        <input type="text" name="nombre" placeholder="Nombre y apellido" class="formInput" required>
+        
+        <label>Teléfono: <span>*</span></label>
+        <input type="email" name="telefono" placeholder="Telefeno" class="formInput" required>
+        
+        <label>Email: <span>*</span></label>
+        <input type="email" name="email" placeholder="Email" class="formInput" required>
+
         <input id="inputHidden" name="inputHidden" type="hidden" value="">
         <input id="totalHidden" name="totalHidden" type="hidden" value="">
         <input name="inputConfirmar" type="submit" class="buttonFinalizacion buttonFinalizacionAzul" value="Confirmar">
     </form>
+
 
 <!-- HEADER -->
     <footer>
