f8d4da9c879b8eefea0e6482f17851ecc0be0bf1
PabloGomezB <a18pabgombra@inspedralbes.cat>
2020-11-03 22:50:03 +0100

    administracion muestra todas las comandas

--- admin/admin.php
+++ admin/admin.php
@@ -9,46 +9,98 @@ ob_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pantalla de administración</title>
 
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
-    <?php include("include/includes.inc") ?>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link href="../libs/fontawesome5/css/all.min.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-    <!-- HEADER -->
     <header>
-        <?php include("include/header.php"); ?>
+        <nav class="navbar navbar-expand-md navbar-light fixed-top" style="background-color: #e3f2fd;">
+            <a class="navbar-brand" href="../index.php"><img src="../img/logo.png" width="130" height="60"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
     </header>
 
     <h1>Mostrando comandas</h1>
 
     <?php
+    
+        $botones = '<input name="comanda" type="submit" class="display comanda" value="';
+        $ruta = "../comandas/";
 
+        // scandir: Escanea la ruta y devuelve todo su contenido (en sistemas Lunix devuelve "." y ".." tambien)
+        // array_diff: Elimina los elementos del array que indiques en este caso array('..', '.').
+        // array_slice: Reordena el array ya que al hacer array_diff las posiciones del array se mantienen intactas
+        $ficheros = array_slice(array_diff(scandir($ruta), array('..', '.')), 0);
 
+        if (empty($ficheros)){
+            echo "No hay comandas";
+        }
+        else{
+    ?>
 
-        $ruta = "../comandas";
+        <div class="div-left">
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
 
-        function dir_is_empty($ruta) {
-            $handle = opendir($ruta);
-            while (false !== ($entry = readdir($handle))) {
-              if ($entry != "." && $entry != "..") {
-                closedir($handle);
-                return FALSE;
-              }
-            }
-            closedir($handle);
-            return TRUE;
+    <?php
+
+        foreach ($ficheros as $comanda) {
+            echo '<div class="celda">';
+            echo $botones . $comanda . '"</input>';
+            echo '</div>';
         }
 
-        print_r(dir_is_empty($ruta));
+        echo '</form>';
+        echo '</div>';
+        
+        
+        echo '<div class="div-right">';
+
+        $rutaComandaSeleccionada = $ruta . $_POST['comanda'];
+        
+        echo nl2br(file_get_contents($rutaComandaSeleccionada));
+        
+        echo '</div>';
+    }
+
+        //print_r($ficheros);
+        
 
     ?>
 
-    <!-- HEADER -->
     <footer>
-        <?php include("include/footer.php"); ?>
+        <table class="centrar">
+            <td>
+                <a target="_blank" href="http://www.institutpedralbes.cat/">&copy; 2020 Institut Pedralbes</a> &middot;<a href="#">Términos y Condiciones</a>
+            </td>
+            <td>
+                <ul>
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                        <i class="fab fa-facebook fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                        <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                        <i class="fab fa-instagram fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                </ul>
+            </td>
+            <td>
+                <a class="float-right" href="#">Ir arriba</a>
+        </td>
+        </table>
     </footer>
 </body>
 
