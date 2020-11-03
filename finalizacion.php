6e78994798ba33e9c4489d285c37c23fedf7d88a
kevinlarriega <a18kevlarpal@inspedralbes.cat>
2020-11-03 20:32:29 +0100

    Arreglé un problema con el mergeo

--- finalizacion.php
+++ finalizacion.php
@@ -29,27 +29,23 @@
         <br>
 
         <h3>Productos del carrito</h3>
-    
-    
-
         
-  
-    <form action="confirmacion.php" method="POST"  class="form">
-        <img src="img/finalizacion/contacto.png" class="formImg"/>
-        <label>Nombre y apellidos: <span>*</span></label>
-        <input type="text" name="nombre" placeholder="Nombre y apellido" class="formInput" required>
-        
-        <label>Teléfono: <span>*</span></label>
-        <input type="number" name="telefono" placeholder="Telefeno" class="formInput" required>
-        
-        <label>Email: <span>*</span></label>
-        <input type="email" name="email" placeholder="Email" class="formInput" required>
+        <form action="confirmacion.php" method="POST"  class="form">
+            <img src="img/finalizacion/contacto.png" class="formImg"/>
+            <label>Nombre y apellidos: <span>*</span></label>
+            <input type="text" name="nombre" placeholder="Nombre y apellido" class="formInput" required>
+            
+            <label>Teléfono: <span>*</span></label>
+            <input type="number" name="telefono" placeholder="Telefeno" class="formInput" required>
+            
+            <label>Email: <span>*</span></label>
+            <input type="email" name="email" placeholder="Email" class="formInput" required>
 
-        <input id="inputHidden" name="inputHidden" type="hidden" value="">
-        <input id="totalHidden" name="totalHidden" type="hidden" value="">
-        <input name="inputConfirmar" type="submit" class="buttonFinalizacion buttonFinalizacionAzul" value="Confirmar">
-    </form>
-
+            <input id="inputHidden" name="inputHidden" type="hidden" value="">
+            <input id="totalHidden" name="totalHidden" type="hidden" value="">
+            <input name="inputConfirmar" type="submit" class="buttonFinalizacion buttonFinalizacionAzul" value="Confirmar">
+        </form>
+    </div>
 <!-- HEADER -->
     <footer id="findex">
         <?php include("include/footer.php"); ?>
